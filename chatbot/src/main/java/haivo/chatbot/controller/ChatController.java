a60ab8d8629ff856825c182dc7d588db69614367
haivoDA22TTD <vochihai7@gmail.com>
2025-10-12 14:43:21 +0700

    Update Endpoint chat-with-image

--- chatbot/src/main/java/haivo/chatbot/controller/ChatController.java
+++ chatbot/src/main/java/haivo/chatbot/controller/ChatController.java
@@ -4,7 +4,9 @@ import haivo.chatbot.dto.ChatRequest;
 import haivo.chatbot.service.ChatService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class ChatController {
@@ -16,5 +18,10 @@ public class ChatController {
     @PostMapping("/chat")
     public String chat(@RequestBody ChatRequest request) {
         return chatService.chat(request);
+    }
+    @PostMapping("/chat-with-image")
+    public String chatWithImage(@RequestParam ("file") MultipartFile file,
+                                @RequestParam ("message") String message){
+        return chatService.chatWithImage(file, message);
     }
 }
--- chatbot/src/main/java/haivo/chatbot/service/ChatService.java
+++ chatbot/src/main/java/haivo/chatbot/service/ChatService.java
@@ -2,19 +2,49 @@ package haivo.chatbot.service;
 
 import haivo.chatbot.dto.ChatRequest;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.content.Media;
 import org.springframework.stereotype.Service;
+import org.springframework.util.MimeType;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class ChatService {
     private final ChatClient chatClient;
+
     public ChatService(ChatClient.Builder clientBuilder) {
         chatClient = clientBuilder.build();
     }
 
     public String chat(ChatRequest request) {
-       return chatClient
-               .prompt(request.message())
-               .call()
-               .content();
+        SystemMessage systemMessage = new SystemMessage("""
+                Hey there! I'm haivoDev, your friendly virtual assistant 🤖✨
+                                        Here to help you with anything you need, just ask away!
+                """);
+        UserMessage userMessage = new UserMessage(request.message());
+        Prompt prompt = new Prompt(systemMessage, userMessage);
+        return chatClient
+                .prompt(prompt)
+                .call()
+                .content();
+    }
+
+    public String chatWithImage(MultipartFile file, @RequestParam String message) {
+        Media media = Media.builder()
+                .mimeType(MimeTypeUtils.parseMimeType(file.getContentType()))
+                .data(file.getResource())
+                .build();
+        return chatClient.prompt()
+                .system("Hey there! I'm haivoDev, your friendly virtual assistant 🤖✨")
+                .user(promptUserSpec
+                -> promptUserSpec.media(media)
+                .text(message))
+                .call()
+                .content();
     }
 }
+
